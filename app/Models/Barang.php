517b362c170caf62b454650377194bf3dbbf82cd
fransmissah <dandimissah19@gmail.com>
2025-05-22 12:39:52 +0700

    Update Barang.php

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    //
+    protected $fillable = [
+        'kode_barang',
+        'nama_barang',
+        'kategori',
+        'stok',
+        'harga',
+        // tambahkan kolom lain yang ada di form dan tabel
+    ];
 }
